c99210b250e2fc1a8caf3a6468cdcda1df64c33a
AFROUKH oussama <65024956+ous47@users.noreply.github.com>
2024-06-17 23:09:53 +0100

    Tokito Muichiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { MacarouselComponent } from './macarousel/macarousel.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MacarouselComponent,
+    MenuComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    PageNotFoundComponent,
+    
   ],
-  imports: [
+  imports: [ CarouselModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [
     provideClientHydration()
